dd19e24ab90415dca63e01b919b90fedda5d747d
Al Nwoks <chalonnwokedike@gmail.com>
2021-03-14 01:38:48 +0100

    updated buildspec.yaml file and added nginx config and install scripts

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Installing dependencies..."
+sudo apt-get -y update
+sudo apt-get install -y nginx
+sudo apt-get install -y maven
+sudo apt-get install -y default-jdk
+sudo apt-get install -y wget
+
+echo "Setting up NGINX..." 
+sudo wget -o https://github.com/alnwoks/geostore/tree/master/nginx/default.conf default.conf
+sudo mv default.conf /etc/nginx/default.conf
