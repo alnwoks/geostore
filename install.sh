0c02908a9f395e03ac0fb27af44be01ffbf2f6d5
Al Nwoks <chalonnwokedike@gmail.com>
2021-03-14 14:51:05 +0100

    updated buildspec.yaml file

--- install.sh
+++ install.sh
@@ -8,6 +8,14 @@ sudo apt-get install -y default-jdk
 sudo apt-get install -y wget
 
 echo "Setting up NGINX..." 
-sudo wget -o https://github.com/alnwoks/geostore/tree/master/nginx/default.conf nginx.conf
+sudo wget -o https://github.com/alnwoks/geostore/tree/master/nginx/default.conf
 sudo mv default.conf /etc/nginx/nginx.conf
 sudo ngimx -s reload
+
+echo "Setting up AWS Code deploy agent..."
+sudo apt-get install -y ruby
+sudo wget https://aws-codedeploy-us-east-2.s3.us-east-2.amazonaws.com/latest/install
+sudo mv install /home/ubuntu/install
+sudo chmod +x /home/ubuntu/install
+sudo /home/ubuntu/install auto > /tmp/logfile
+sudo systemctl start codedeploy-agent   
