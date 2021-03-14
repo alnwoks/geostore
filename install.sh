710b123d7c3ab4e8b4306584ef85e38ea36708a3
Al Nwoks <chalonnwokedike@gmail.com>
2021-03-14 12:22:15 +0100

    updated buildspec.yaml file

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ sudo apt-get install -y default-jdk
 sudo apt-get install -y wget
 
 echo "Setting up NGINX..." 
-sudo wget -o https://github.com/alnwoks/geostore/tree/master/nginx/default.conf default.conf
-sudo mv default.conf /etc/nginx/default.conf
+sudo wget -o https://github.com/alnwoks/geostore/tree/master/nginx/default.conf nginx.conf
+sudo mv default.conf /etc/nginx/nginx.conf
